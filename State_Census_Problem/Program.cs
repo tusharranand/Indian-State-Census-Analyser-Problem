8d1ab4c424ff6cd8986721d57382209ff944762a
Tushar Anand <tusharranand@gmail.com>
2022-04-13 08:19:46 +0530

    Introduction

---
+++ State_Census_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace State_Census_Problem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser Problem");
+        }
+    }
+}
